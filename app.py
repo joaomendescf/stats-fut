2aa915aaf9e802d4682774a0515a8e075b0f69b5
joaomendescf <111872868+joaomendescf@users.noreply.github.com>
2023-03-22 12:03:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -111,14 +111,18 @@ st.dataframe(df)
 
 
 
-def convert_df_xlsx(df):
-    # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
-    towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
-    towrite.seek(0)  # reset pointer
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
-    return st.markdown(href, unsafe_allow_html=True)
+def convert_df_xlsx(df, extension = 'xlsx'):
+    if extension == 'csv': # csv 
+        csv = df.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode() 
+    else: # pickle
+        b = io.BytesIO()
+        pickle.dump(df, b)
+        b64 = base64.b64encode(b.getvalue()).decode()
+    
+    href = f'<a href="data:file/csv;base64,{b64}" download="new_file.{extension}">Download {extension}</a>'
+    st.write(href, unsafe_allow_html=True) 
+
 
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
@@ -127,7 +131,6 @@ def convert_df(df):
  # -- DOWNLOAD SECTION
 
 st.subheader('Downloads:')
-# generate_excel_download_link(df_grouped)
 convert_df_xlsx(df)
 
 st.download_button(
