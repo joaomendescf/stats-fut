b32a5c5c7224e1aa834a8f75340d16c46ad4a3f4
joaomendescf <111872868+joaomendescf@users.noreply.github.com>
2023-03-22 10:23:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import xlsxwriter
 from io import BytesIO
 import pandas as pd
 from pandas import json_normalize
+import xlsxwriter
 import base64
 from datetime import datetime, timedelta, date
 
